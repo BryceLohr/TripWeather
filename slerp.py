daf7c8aa95a4ed52077cde4c0e81f7c7da22457f
Bryce Lohr <bryce.lohr@gmail.com>
2015-02-08 18:31:40 -0500

    Added another example route to play with

--- slerp.py
+++ slerp.py
@@ -2,12 +2,14 @@ from pysal.cg import sphere
 
 jfk = (-73.778925,40.639751)
 sfo = (-122.374889,37.618972)
+gso = (-79.937306,36.09775)
+mia = (-80.290556,25.79325)
 mph = 300
 
-miles = sphere.arcdist(jfk, sfo, sphere.RADIUS_EARTH_MILES)
+miles = sphere.arcdist(gso, mia, sphere.RADIUS_EARTH_MILES)
 hours = miles / mph
 
-print "Reach SFO from JFK in {0} hours at {1} mph".format(hours, mph)
+print "Reach MIA from GSO in {0} hours at {1} mph".format(hours, mph)
 print "Hourly weather forecast coordinates:"
 
 # WeatherSource only returns hourly forecasts on the hour
