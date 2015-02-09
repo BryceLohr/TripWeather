0c8975cf97845b9dcbd3b64f7f41782dc58873fc
Bryce Lohr <bryce.lohr@gmail.com>
2015-02-08 20:25:53 -0500

    Configured for Heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn TripWeather.wsgi --log-file -
--- TripWeather/settings.py
+++ TripWeather/settings.py
@@ -55,12 +55,10 @@ WSGI_APPLICATION = 'TripWeather.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
+import dj_database_url
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config()
 }
 
 # Internationalization
@@ -80,4 +78,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
+
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
