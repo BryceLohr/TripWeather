a2fb8cbf12bf29af82dd96c54187993b8d3c6889
Bryce Lohr <bryce.lohr@gmail.com>
2015-02-10 23:38:27 -0500

    Put in mock content for weather resources

--- TripWeather/urls.py
+++ TripWeather/urls.py
@@ -12,8 +12,8 @@ urlpatterns = patterns('',
     url(r'^$', 'api.views.index'),
 
     url(r'^flight_plan$', FlightPlan.as_view()),
-    url(r'^weather_for_route$', WeatherForRoute.as_view())
-    url(r'^weather_for_route/(?P<forecast_id>\d+)$', WeatherForRouteDetail.as_view())
+    url(r'^weather_for_route$', WeatherForRoute.as_view()),
+    url(r'^weather_for_route/(\d+)$', WeatherForRouteDetail.as_view(), name='weather_detail'),
 
     url(r'^admin/', include(admin.site.urls)),
 )
