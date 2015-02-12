1eadce7263d4a531997f459b972f120b9636920d
Bryce Lohr <bryce.lohr@gmail.com>
2015-02-11 20:40:02 -0500

    Added data migration to populate Airports table

--- TripWeather/settings.py
+++ TripWeather/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = (
     'restless',
     'bootstrap3',
     'djangular',
+    'ui',
     'api',
 )
 
--- ui/models.py
+++ ui/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Airport(models.Model):
+    """
+    Represents the OpenFlights.org Airports database. See
+    http://openflights.org/data.html
+    """
+    name = models.CharField(max_length=75)
+    city = models.CharField(max_length=75)
+    country = models.CharField(max_length=75)
+    iata_faa = models.CharField(max_length=3, db_index=True, null=True, blank=True)
+    icao = models.CharField(max_length=4, null=True, blank=True)
+    latitude = models.DecimalField(max_digits=9, decimal_places=6)
+    longitude = models.DecimalField(max_digits=9, decimal_places=6)
+    altitude = models.IntegerField(help_text="In feet")
+    timezone = models.DecimalField(max_digits=3, decimal_places=1, help_text="Offset from UTC in hours")
+    dst_code = models.CharField(max_length=1)
+    tz_name = models.CharField(max_length=75, help_text="Name from the Olson timezone database")
+
+
