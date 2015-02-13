fc395d9d0dd1c6cc9ae9b1130c6bc16957e835d4
Bryce Lohr <bryce.lohr@gmail.com>
2015-02-13 17:59:38 -0500

    Fixed config for Heroku

--- TripWeather/settings.py
+++ TripWeather/settings.py
@@ -99,7 +99,5 @@ BOOTSTRAP3 = {
 }
 
 # Local dev environment settings
-try:
-    from local_settings import *
-except ImportError:
-    pass
+GOOGLE_API_KEY = os.environ['GOOGLE_API_KEY']
+WEATHERSOURCE_API_KEY = os.environ['WEATHERSOURCE_API_KEY']
